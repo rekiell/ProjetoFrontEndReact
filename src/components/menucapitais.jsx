a7dde6079509bc82b69077b03f6b0669187a60a2
Rekiell <gabriel.majewski@gmail.com>
2022-10-23 15:35:52 -0300

    Adicionei array de cidades requisitadas e fiz map

--- src/components/menucapitais.jsx
+++ src/components/menucapitais.jsx
@@ -1,9 +1,18 @@
 function MenuCapitais () {
+    const cidades = ["Rio de janeiro", "São Paulo", "Belo Horizonte", "Brasília", "Belém",
+    "Salvador", "Curitiba", "Fortaleza", "Manaus", "João Pessoa"]
+    const listaCidades = cidades.map ((cidadestemp)=>
+        <li> {cidadestemp} </li>
+    )
     return (
         <main>
             <div className="listaDasCapitais">
             <hr/>
             <h2>Capitais</h2>
+            <p>Min Máx</p>
+            </div>
+            <div className="lista_cidades">
+                <ul> {listaCidades} </ul>
             </div>
         </main>
 
