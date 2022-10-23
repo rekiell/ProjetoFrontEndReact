d1b9aa09e5b8b8ca1b1b14c41017ba3578fc146c
Rekiell <gabriel.majewski@gmail.com>
2022-10-23 10:40:02 -0300

    Botão de Pesquisa funcional

--- src/components/apitech.jsx
+++ src/components/apitech.jsx
@@ -1,5 +1,5 @@
     async function temperaturaDasCapitais(cidade){
-        const requisicao = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cidade + '&appid=')
+        const requisicao = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cidade + '&appid=&units=metric')
         
         const cidadeEncontrada = await requisicao.json();
         
@@ -16,16 +16,19 @@
             .then( capital => {
     
                 const cabecalho = document.getElementById('root');
-    
+                
+                const temperaturaMin = Math.round(capital.main.temp_min)
+                const temperaturaMax = Math.round(capital.main.temp_max)
+
                 cabecalho.innerHTML += `
                     <p>
                          Cidade: ${capital.name} 
                     </p>
                     <p>
-                        Temperatura min: ${capital.main.temp_min}
+                        Temperatura min: ${temperaturaMin + "°"}
                     </p>
                     <p>
-                        Temperatura max: ${capital.main.temp_max}
+                        Temperatura max: ${temperaturaMax + "°"}
                     </p>    
                 `
     
