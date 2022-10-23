70d0bb008bd67c765d6cbe9661d3967917b4ba1f
Rekiell <gabriel.majewski@gmail.com>
2022-10-23 15:34:29 -0300

    Mudei o elemento da const cabecalho e o innerHTML

--- src/components/apitech.jsx
+++ src/components/apitech.jsx
@@ -1,39 +1,33 @@
-    async function temperaturaDasCapitais(cidade){
-        const requisicao = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cidade + '&appid=&units=metric')
-        
-        const cidadeEncontrada = await requisicao.json();
-        
-        return cidadeEncontrada;
-        
-    }
+async function temperaturaDasCapitais(cidade) {
+  const requisicao = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cidade +
+      "&appid=&units=metric"
+  );
 
-    function pesquisaCidade(){
-        
-        const elementoCidade = document.getElementById('inputcidade');
-        const cidade = elementoCidade.value;
-        
-        temperaturaDasCapitais(cidade)
-            .then( capital => {
-    
-                const cabecalho = document.getElementById('root');
-                
-                const temperaturaMin = Math.round(capital.main.temp_min)
-                const temperaturaMax = Math.round(capital.main.temp_max)
+  const cidadeEncontrada = await requisicao.json();
 
-                cabecalho.innerHTML += `
-                    <p>
+  return cidadeEncontrada;
+}
+
+function pesquisaCidade() {
+  const elementoCidade = document.getElementById("inputcidade");
+  const cidade = elementoCidade.value;
+  temperaturaDasCapitais(cidade).then((capital) => {
+    const cabecalho = document.getElementById("pequisa_completa");
+    const temperaturaMin = Math.round(capital.main.temp_min);
+    const temperaturaMax = Math.round(capital.main.temp_max);
+
+    cabecalho.innerHTML += `
+                    <h2>
                          Cidade: ${capital.name} 
+        
+                    </h2>
+                        Min: ${temperaturaMin + "°"}
                     </p>
                     <p>
-                        Temperatura min: ${temperaturaMin + "°"}
-                    </p>
-                    <p>
-                        Temperatura max: ${temperaturaMax + "°"}
+                        Max: ${temperaturaMax + "°"}
                     </p>    
-                `
-    
-            });
-    
-    }
-
-
+                `;
+  });
+}
